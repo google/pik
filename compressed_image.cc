41e7a3cf1dcfd83ee7799d013430ffa9d53300f3
Dominik Homberger <dominikhlbg@gmail.com>
2017-07-25 14:57:48 +0200

    Revert "Add _CRT_SECURE_NO_WARNINGS flags"
    This reverts commit 249730ad7446f4c12306d26d4aa8126e47272d8c.

--- compressed_image.cc
+++ compressed_image.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "compressed_image.h"
 
 #include <math.h>
--- image_io.cc
+++ image_io.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "image_io.h"
 #include <stddef.h>
 #include <stdint.h>
