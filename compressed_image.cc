bd42829bf11e9e6508d713994758a0b033280a24
Dominik Homberger <dominikhlbg@gmail.com>
2017-07-25 14:59:57 +0200

    Add _CRT_SECURE_NO_WARNINGS flags

--- compressed_image.cc
+++ compressed_image.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "compressed_image.h"
 
 #include <math.h>
--- cpik.cc
+++ cpik.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "image_io.h"
 #include "pik.h"
 
